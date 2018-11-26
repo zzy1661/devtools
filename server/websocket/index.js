975384a72b010fd9d568f9aa21febb9895eb3ee4
zzy1661 <1556597051@qq.com>
2018-11-26 21:48:56 +0800

    添加ws

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -1,1 +1,20 @@
-// var io = require('socket.io')();
+var express = require('express');
+var socketApp = express();
+var socketServer = require('http').Server(socketApp);
+var io = require('socket.io')(socketServer);
+io.on('connection', function(socket) {
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+	socket.on('chat message', function(msg) {
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
+	io.emit('some event', { for: 'everyone' });
+	socket.broadcast.emit('hi','broadcast');
+
+});
+
+socketServer.listen(8080, function() {
+	console.log('socket listening on *:8080');
+});
